f15037b4a7c6722f4eaeecc969926ceb2ab32795
Hailey Hurt <hhurt@usc.edu>
2015-11-18 20:27:29 -0800

    added IP class

---
+++ src/utilities/IP.java
@@ -0,0 +1,26 @@
+package utilities;
+
+public class IP
+{
+    private String address;
+    private int port;
+    
+    public int getPort()
+    {
+        return address;
+    }
+    public void setPort(int p)
+    {
+        port = p;
+    }
+    
+    public String getAddress()
+    {
+        return address;
+    }
+    public void setAddress(String add)
+    {
+        address = add;
+    }
+}
+
